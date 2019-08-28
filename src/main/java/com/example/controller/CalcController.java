da8080d7a41be964acb0734eca8364c3ba3f05c4
jurisaito <juri.saito@rakus-partners.co.jp>
2019-08-28 10:53:54 +0900

    first commit

---
+++ src/main/java/com/example/controller/CalcController.java
@@ -0,0 +1,59 @@
+package com.example.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.logic.CalcLogic;
+
+@Controller
+@RequestMapping("/calc")
+public class CalcController {
+
+	@Autowired
+	private CalcLogic calcLogic;
+	
+	@RequestMapping("/add")
+	public String add() {
+		int num1 = 2;
+		int num2 = 3;
+		
+		int resultOfAdd = calcLogic.add(num1, num2);
+		
+		System.out.println(num1 + "+" + num2 + "=" + resultOfAdd);
+		return "finished";
+	}
+	
+	@RequestMapping("/sub")
+	public String sub() {
+		int num1 = 3;
+		int num2 = 2;
+		
+		int resultOfSub = calcLogic.sub(num1, num2);
+		
+		System.out.println(num1 + "-" +num2 + "=" + resultOfSub);
+		return "finished";
+	}
+	
+	@RequestMapping("/multi")
+	public String multi() {
+		int num1 = 3;
+		int num2 = 2;
+		
+		int resultOfMulti = calcLogic.multi(num1, num2);
+		
+		System.out.println(num1 + "*" + num2 + "=" + resultOfMulti);
+		return "finished";
+	}
+	
+	@RequestMapping("/div")
+	public String div() {
+		int num1 = 4;
+		int num2 = 2;
+		
+		int resultOfDiv = calcLogic.div(num1, num2);
+		
+		System.out.println(num1 + "/" + num2 + "=" + resultOfDiv);
+		return "finished";
+	}
+}
---
+++ src/main/java/com/example/logic/CalcLogic.java
@@ -0,0 +1,23 @@
+package com.example.logic;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CalcLogic {
+
+	public int add(int num1, int num2) {
+		return num1 + num2;
+	}
+	
+	public int sub(int num1, int num2) {
+		return num1 - num2;
+	}
+	
+	public int multi(int num1, int num2) {
+		return num1 * num2;
+	}
+	
+	public int div(int num1, int num2) {
+		return num1 / num2;
+	}
+}
